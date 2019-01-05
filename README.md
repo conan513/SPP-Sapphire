2401f6347e72db01488e48282c3db1fc354bc4bc
ConanHUN <conan_513@hotmail.com>
2019-01-05 15:02:39 +0100

    Release

---
+++ README.md
@@ -0,0 +1,4 @@
+# SPP-Sapphire
+
+A Final Fantasy XIV 4.0+ Server Emulator written in C++ 
+https://github.com/SapphireServer/Sapphire
---
+++ tools/install_nuget_packages.bat
@@ -0,0 +1,9 @@
+nuget install mysqlconnector -version 0.47.1
+nuget install Microsoft.EntityFrameworkCore -version 2.1.4
+nuget install Microsoft.EntityFrameworkCore.Design -version 2.1.4
+nuget install NLog -version 4.5.10
+nuget install NLog.Extensions.Logging -version 1.3.0
+nuget install NLog.Web.AspNetCore -version 4.7.0
+nuget install Pomelo.EntityFrameworkCore.MySql -version 2.1.4
+nuget install Pomelo.JsonObject -version 2.0.0
+pause
